739be3fa404238b439e882bc9f8b1d5d6c916aad
project-231 <167600997+project-231@users.noreply.github.com>
2024-05-07 02:10:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const staffButton = document.getElementById("button2");
 
     staffButton.addEventListener("click", function() {
-        window.location.href = "staff.html"; // Redirect to the staff page
+        window.location.href = "staff_login.html"; // Redirect to the staff page
     });
 });
 document.addEventListener("DOMContentLoaded", function() {
